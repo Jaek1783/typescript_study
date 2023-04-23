90931f21450928f136c8e50689409295f680e51a
jaekeun Choi <jaek1783@gmail.com>
2023-04-23 13:20:10 +0900

    typescript tuple type study

---
+++ app.ts
@@ -0,0 +1,10 @@
+const person : {
+    name : string,
+    age : number,
+    role : [number, string]
+} = {
+    name : 'wormsBrother',
+    age : 30,
+    role : [1,'author']
+};
+console.log(person.name);
